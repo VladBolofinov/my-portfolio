aefd09f5b575aec992e733b7aa248777fa7eb746
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-08-04 19:20:46 +0300

    set up directories tree

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import { MainPage } from "../mainPage/MainPage";
+
+function App() {
+
+  return (
+    <>
+        <MainPage/>
+        <div className='block'></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/mainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+export const MainPage = () => {
+    return (
+        <>
+            <div className="greetings">
+                Hi, my name is Vladislav Bolofinov! I'm a frontend-developer!
+            </div>
+        </>
+    )
+}
